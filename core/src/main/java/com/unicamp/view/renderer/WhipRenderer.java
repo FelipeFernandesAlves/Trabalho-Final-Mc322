a6546fcb30d4f98e32c07c5a4fb4ff701ced9708
Saulo <castro1799804@gmail.com>
2026-07-12 06:31:56 -0300

    Adição dos sprites de animações do player e whip

--- core/src/main/java/com/unicamp/view/renderer/WhipRenderer.java
+++ core/src/main/java/com/unicamp/view/renderer/WhipRenderer.java
@@ -2,22 +2,44 @@ package com.unicamp.view.renderer;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.unicamp.model.entity.projectile.WhipProjectile;
 import com.unicamp.view.EntityRenderer;
 
 public class WhipRenderer implements EntityRenderer<WhipProjectile> {
 
-	Texture texture = new Texture(Gdx.files.internal("cachorro.png"));
+	private final Animation<TextureRegion> idleAnimation;
+	private final Texture idleSheet;
+
+	private static final float RENDER_WIDTH = 128f; 
+    private static final float RENDER_HEIGHT = 128f;
+
+	private Animation<TextureRegion> currentAnimation;
+	private float stateTime;
+
+	public WhipRenderer() {
+		idleSheet = new Texture(Gdx.files.internal("Slash_Merge.png"));
+		this.idleAnimation = createAnimation(idleSheet, 6, 1, 0.25f);
+		this.currentAnimation = this.idleAnimation;
+	}
 
 	@Override
 	public void render(SpriteBatch batch, WhipProjectile entity) {
-		batch.draw(texture, entity.getX(), entity.getY());
+		stateTime += Gdx.graphics.getDeltaTime();
+		TextureRegion currentFrame = currentAnimation.getKeyFrame(stateTime, true);
+		batch.draw(currentFrame, 
+                   entity.getX() - RENDER_WIDTH / 2f,  
+                   entity.getY() - RENDER_HEIGHT / 2f, 
+                   2*RENDER_WIDTH,                      
+                   RENDER_HEIGHT                      
+        );
 	}
 
 	@Override
 	public void dispose() {
-		texture.dispose();
+		idleSheet.dispose();
 	}
 	
 }
